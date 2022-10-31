4c929042452d41027ad2f8c41e483cdbd50fef74
Jan Henrik Weinstock <jan@mwa.re>
2022-10-31 15:38:49 +0100

    Fix copyright headers

--- include/mwr/core/report.h
+++ include/mwr/core/report.h
@@ -1,6 +1,6 @@
 /******************************************************************************
  *                                                                            *
- * Copyright 2018 Jan Henrik Weinstock                                        *
+ * Copyright 2022 MachineWare GmbH                                            *
  *                                                                            *
  * Licensed under the Apache License, Version 2.0 (the "License");            *
  * you may not use this file except in compliance with the License.           *
