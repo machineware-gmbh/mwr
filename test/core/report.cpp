3b9375213137a971c7a132dc2f2ec5a8aa81beaf
Jan Henrik Weinstock <jan@mwa.re>
2023-04-17 16:44:00 +0200

    Adding report test for message, file and line number

--- test/core/report.cpp
+++ test/core/report.cpp
@@ -19,6 +19,17 @@
 #include "testing.h"
 #include "mwr/core/report.h"
 
+TEST(report, what) {
+    const char* what = "test report";
+    const char* file = __FILE__;
+    const size_t line = __LINE__;
+
+    mwr::report rep(what, file, line);
+    EXPECT_STREQ(rep.what(), what);
+    EXPECT_STREQ(rep.file(), file);
+    EXPECT_EQ(rep.line(), line);
+}
+
 TEST(report, report) {
     EXPECT_THROW(MWR_REPORT("test %d", 123), mwr::report);
     EXPECT_THROW(MWR_REPORT_ON(true, "test %d", 456), mwr::report);
