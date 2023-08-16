844a45d6a5c869c4644363649b302594005746d3
Jan Henrik Weinstock <jan@mwa.re>
2023-08-16 10:49:39 +0100

    Enable elf test

--- test/resources/shared.c
+++ test/resources/shared.c
@@ -1,7 +1,16 @@
-int global = 42;
+#ifdef _MSC_VER
+#define API __declspec(dllexport)
+#else
+#define API
+#endif
 
-int function(int arg) {
+API int global = 42;
+
+API int function(int arg) {
     return arg + global;
 }
 
 // gcc -fPIC -shared -Wl,-soname,shared.so -o shared.so shared.c
+
+// cl.exe /c shared.c
+// link.exe /DLL /OUT:shared.dll shared.obj
