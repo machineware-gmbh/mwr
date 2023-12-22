bd7b8a66c48feaa0b98c9415945ad5a4b853c30f
Jan Henrik Weinstock <jan@mwa.re>
2023-12-22 12:00:10 +0100

    Added extended tests for atomics

--- include/mwr/core/atomics.h
+++ include/mwr/core/atomics.h
@@ -27,50 +27,50 @@ inline void barrier() {
 #endif
 }
 
-inline void read_once(void* dest, const void* src, size_t n) {
+inline void read_once(void* dest, const volatile void* src, size_t n) {
     switch (n) {
     case 1:
-        *(u8*)dest = *(volatile u8*)src;
+        *(u8*)dest = *(const volatile u8*)src;
         break;
     case 2:
-        *(u16*)dest = *(volatile u16*)src;
+        *(u16*)dest = *(const volatile u16*)src;
         break;
     case 4:
-        *(u32*)dest = *(volatile u32*)src;
+        *(u32*)dest = *(const volatile u32*)src;
         break;
     case 8:
-        *(u64*)dest = *(volatile u64*)src;
+        *(u64*)dest = *(const volatile u64*)src;
         break;
     default:
         barrier();
-        memcpy(dest, src, n);
+        memcpy(dest, (const void*)src, n);
         barrier();
     }
 }
 
-inline void write_once(void* dest, const void* src, size_t n) {
+inline void write_once(volatile void* dest, const void* src, size_t n) {
     switch (n) {
     case 1:
-        *(volatile u8*)dest = *(u8*)src;
+        *(volatile u8*)dest = *(const u8*)src;
         break;
     case 2:
-        *(volatile u16*)dest = *(u16*)src;
+        *(volatile u16*)dest = *(const u16*)src;
         break;
     case 4:
-        *(volatile u32*)dest = *(u32*)src;
+        *(volatile u32*)dest = *(const u32*)src;
         break;
     case 8:
-        *(volatile u64*)dest = *(u64*)src;
+        *(volatile u64*)dest = *(const u64*)src;
         break;
     default:
         barrier();
-        memcpy(dest, src, n);
+        memcpy((void*)dest, src, n);
         barrier();
     }
 }
 
 template <typename T>
-inline T read_once(const void* src) {
+inline T read_once(const volatile void* src) {
     T val = 0;
     read_once(&val, src, sizeof(T));
     return val;
@@ -81,26 +81,264 @@ inline void write_once(void* dest, const T& val) {
     write_once(dest, &val, sizeof(val));
 }
 
-template <typename T, typename T2 = T, typename T3 = T>
-inline bool atomic_cmpxchg(T* ptr, T2 expected, T3 desired) {
-#ifndef MWR_MSVC
-    return __atomic_compare_exchange(ptr, (T*)&expected, (T*)&desired, false,
-                                     __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST);
+inline u8 atomic_and8(volatile void* mem, u8 val) {
+#ifdef MWR_MSVC
+    return _InterlockedAnd8((volatile char*)mem, (char)val);
 #else
-    if constexpr (sizeof(T) == sizeof(char))
-        return _InterlockedCompareExchange8((volatile char*)ptr, (char)desired,
-                                            (char)expected);
-    if constexpr (sizeof(T) == sizeof(short))
-        return _InterlockedCompareExchange16((volatile short*)ptr,
-                                             (short)desired, (short)expected);
-    if constexpr (sizeof(T) == sizeof(long))
-        return _InterlockedCompareExchange((volatile long*)ptr, (long)desired,
-                                           (long)expected);
-    if constexpr (sizeof(T) == sizeof(long long))
-        return _InterlockedCompareExchange64(
-            (volatile long long*)ptr, (long long)desired, (long long)expected);
-    MWR_UNREACHABLE;
+    return __atomic_fetch_and((volatile u8*)mem, val, __ATOMIC_SEQ_CST);
 #endif
+}
+
+inline u16 atomic_and16(volatile void* mem, u16 val) {
+#ifdef MWR_MSVC
+    return _InterlockedAnd16((volatile short*)mem, (short)val);
+#else
+    return __atomic_fetch_and((volatile u16*)mem, val, __ATOMIC_SEQ_CST);
+#endif
+}
+
+inline u32 atomic_and32(volatile void* mem, u32 val) {
+#ifdef MWR_MSVC
+    return _InterlockedAnd((volatile long*)mem, (long)val);
+#else
+    return __atomic_fetch_and((volatile u32*)mem, val, __ATOMIC_SEQ_CST);
+#endif
+}
+
+inline u64 atomic_and64(volatile void* mem, u64 val) {
+#ifdef MWR_MSVC
+    return _InterlockedAnd64((volatile long long*)mem, (long long)val);
+#else
+    return __atomic_fetch_and((volatile u64*)mem, val, __ATOMIC_SEQ_CST);
+#endif
+}
+
+inline u64 atomic_and(volatile void* mem, u64 val, size_t size) {
+    switch (size) {
+    case 1:
+        return atomic_and8(mem, (u8)val);
+    case 2:
+        return atomic_and16(mem, (u16)val);
+    case 4:
+        return atomic_and32(mem, (u32)val);
+    case 8:
+        return atomic_and64(mem, (u64)val);
+    default:
+        MWR_ERROR("%s: unsupported operand size: %zu bytes", __func__, size);
+    }
+}
+
+template <typename T>
+inline T atomic_and(volatile void* mem, T val) {
+    return (T)atomic_and(mem, val, sizeof(T));
+}
+
+inline u8 atomic_or8(volatile void* mem, u8 val) {
+#ifdef MWR_MSVC
+    return _InterlockedOr8((volatile char*)mem, (char)val);
+#else
+    return __atomic_fetch_or((volatile u8*)mem, val, __ATOMIC_SEQ_CST);
+#endif
+}
+
+inline u16 atomic_or16(volatile void* mem, u16 val) {
+#ifdef MWR_MSVC
+    return _InterlockedOr16((volatile short*)mem, (short)val);
+#else
+    return __atomic_fetch_or((volatile u16*)mem, val, __ATOMIC_SEQ_CST);
+#endif
+}
+
+inline u32 atomic_or32(volatile void* mem, u32 val) {
+#ifdef MWR_MSVC
+    return _InterlockedOr((volatile long*)mem, (long)val);
+#else
+    return __atomic_fetch_or((volatile u32*)mem, val, __ATOMIC_SEQ_CST);
+#endif
+}
+
+inline u64 atomic_or64(volatile void* mem, u64 val) {
+#ifdef MWR_MSVC
+    return _InterlockedOr64((volatile long long*)mem, (long long)val);
+#else
+    return __atomic_fetch_or((volatile u64*)mem, val, __ATOMIC_SEQ_CST);
+#endif
+}
+
+inline u64 atomic_or(volatile void* mem, u64 val, size_t size) {
+    switch (size) {
+    case 1:
+        return atomic_or8(mem, (u8)val);
+    case 2:
+        return atomic_or16(mem, (u16)val);
+    case 4:
+        return atomic_or32(mem, (u32)val);
+    case 8:
+        return atomic_or64(mem, (u64)val);
+    default:
+        MWR_ERROR("%s: unsupported operand size: %zu bytes", __func__, size);
+    }
+}
+
+template <typename T>
+inline T atomic_or(volatile void* mem, T val) {
+    return (T)atomic_or(mem, val, sizeof(T));
+}
+
+inline u8 atomic_xor8(volatile void* mem, u8 val) {
+#ifdef MWR_MSVC
+    return _InterlockedXor8((volatile char*)mem, (char)val);
+#else
+    return __atomic_fetch_xor((volatile u8*)mem, val, __ATOMIC_SEQ_CST);
+#endif
+}
+
+inline u16 atomic_xor16(volatile void* mem, u16 val) {
+#ifdef MWR_MSVC
+    return _InterlockedXor16((volatile short*)mem, (short)val);
+#else
+    return __atomic_fetch_xor((volatile u16*)mem, val, __ATOMIC_SEQ_CST);
+#endif
+}
+
+inline u32 atomic_xor32(volatile void* mem, u32 val) {
+#ifdef MWR_MSVC
+    return _InterlockedXor((volatile long*)mem, (long)val);
+#else
+    return __atomic_fetch_xor((volatile u32*)mem, val, __ATOMIC_SEQ_CST);
+#endif
+}
+
+inline u64 atomic_xor64(volatile void* mem, u64 val) {
+#ifdef MWR_MSVC
+    return _InterlockedXor64((volatile long long*)mem, (long long)val);
+#else
+    return __atomic_fetch_xor((volatile u64*)mem, val, __ATOMIC_SEQ_CST);
+#endif
+}
+
+inline u64 atomic_xor(volatile void* mem, u64 val, size_t size) {
+    switch (size) {
+    case 1:
+        return atomic_xor8(mem, (u8)val);
+    case 2:
+        return atomic_xor16(mem, (u16)val);
+    case 4:
+        return atomic_xor32(mem, (u32)val);
+    case 8:
+        return atomic_xor64(mem, (u64)val);
+    default:
+        MWR_ERROR("%s: unsupported operand size: %zu bytes", __func__, size);
+    }
+}
+
+template <typename T>
+inline T atomic_xor(volatile void* mem, T val) {
+    return (T)atomic_xor(mem, val, sizeof(T));
+}
+
+inline i8 atomic_add8(volatile void* mem, i8 val) {
+#ifdef MWR_MSVC
+    return _InterlockedExchangeAdd8((volatile char*)mem, (char)val);
+#else
+    return __atomic_fetch_add((volatile i8*)mem, val, __ATOMIC_SEQ_CST);
+#endif
+}
+
+inline i16 atomic_add16(volatile void* mem, i16 val) {
+#ifdef MWR_MSVC
+    return _InterlockedExchangeAdd16((volatile short*)mem, (short)val);
+#else
+    return __atomic_fetch_add((volatile i16*)mem, val, __ATOMIC_SEQ_CST);
+#endif
+}
+
+inline i32 atomic_add32(volatile void* mem, i32 val) {
+#ifdef MWR_MSVC
+    return _InterlockedExchangeAdd((volatile long*)mem, (long)val);
+#else
+    return __atomic_fetch_add((volatile i32*)mem, val, __ATOMIC_SEQ_CST);
+#endif
+}
+
+inline i64 atomic_add64(volatile void* mem, i64 val) {
+#ifdef MWR_MSVC
+    return _InterlockedExchangeAdd64((volatile long long*)mem, (long long)val);
+#else
+    return __atomic_fetch_add((volatile i64*)mem, val, __ATOMIC_SEQ_CST);
+#endif
+}
+
+inline i64 atomic_add(volatile void* mem, i64 val, size_t size) {
+    switch (size) {
+    case 1:
+        return atomic_add8(mem, (i8)val);
+    case 2:
+        return atomic_add16(mem, (i16)val);
+    case 4:
+        return atomic_add32(mem, (i32)val);
+    case 8:
+        return atomic_add64(mem, (i64)val);
+    default:
+        MWR_ERROR("%s: unsupported operand size: %zu bytes", __func__, size);
+    }
+}
+
+template <typename T>
+inline T atomic_add(volatile void* mem, T val) {
+    return (T)atomic_add(mem, val, sizeof(T));
+}
+
+inline u8 atomic_swap8(volatile void* mem, u8 val) {
+#ifdef MWR_MSVC
+    return _InterlockedExchange8((volatile char*)mem, (char)val);
+#else
+    return __atomic_exchange_n((volatile u8*)mem, (u8)val, __ATOMIC_SEQ_CST);
+#endif
+}
+
+inline u16 atomic_swap16(volatile void* mem, u16 val) {
+#ifdef MWR_MSVC
+    return _InterlockedExchange16((volatile short*)mem, (short)val);
+#else
+    return __atomic_exchange_n((volatile u16*)mem, (u16)val, __ATOMIC_SEQ_CST);
+#endif
+}
+
+inline u32 atomic_swap32(volatile void* mem, u32 val) {
+#ifdef MWR_MSVC
+    return _InterlockedExchange((volatile long*)mem, (long)val);
+#else
+    return __atomic_exchange_n((volatile u32*)mem, (u32)val, __ATOMIC_SEQ_CST);
+#endif
+}
+
+inline u64 atomic_swap64(volatile void* mem, u64 val) {
+#ifdef MWR_MSVC
+    return _InterlockedExchange64((volatile long long*)mem, (long long)val);
+#else
+    return __atomic_exchange_n((volatile u64*)mem, (u64)val, __ATOMIC_SEQ_CST);
+#endif
+}
+
+inline u64 atomic_swap(volatile void* mem, u64 val, size_t size) {
+    switch (size) {
+    case 1:
+        return atomic_swap8(mem, (u8)val);
+    case 2:
+        return atomic_swap16(mem, (u16)val);
+    case 4:
+        return atomic_swap32(mem, (u32)val);
+    case 8:
+        return atomic_swap64(mem, (u64)val);
+    default:
+        MWR_ERROR("%s: unsupported operand size: %zu bytes", __func__, size);
+    }
+}
+
+template <typename T>
+inline T atomic_swap(volatile void* mem, T val) {
+    return (T)atomic_swap(mem, val, sizeof(T));
 }
 
 inline bool atomic_cas8(volatile void* ptr, const void* cmp, const void* val) {
@@ -218,209 +456,24 @@ inline bool atomic_cas(volatile void* ptr, T cmp, T data) {
     return atomic_cas(ptr, &cmp, &data, sizeof(T));
 }
 
-template <typename T, typename T2 = T>
-inline T atomic_or(T* mem, T2 val) {
-#ifndef MWR_MSVC
-    return __atomic_fetch_or(mem, (T)val, __ATOMIC_SEQ_CST);
-#else
-    if constexpr (sizeof(T) == sizeof(char))
-        return _InterlockedOr8((volatile char*)mem, (char)val);
-    if constexpr (sizeof(T) == sizeof(short))
-        return _InterlockedOr16((volatile short*)mem, (short)val);
-    if constexpr (sizeof(T) == sizeof(long))
-        return _InterlockedOr((volatile long*)mem, (long)val);
-    if constexpr (sizeof(T) == sizeof(long long))
-        return _InterlockedOr64((volatile long long*)mem, (long long)val);
-    MWR_UNREACHABLE;
-#endif
-}
-
-template <typename T, typename T2 = T>
-inline T atomic_xor(T* mem, T2 val) {
-#ifndef MWR_MSVC
-    return __atomic_fetch_xor(mem, (T)val, __ATOMIC_SEQ_CST);
-#else
-    if constexpr (sizeof(T) == sizeof(char))
-        return _InterlockedXor8((volatile char*)mem, (char)val);
-    if constexpr (sizeof(T) == sizeof(short))
-        return _InterlockedXor16((volatile short*)mem, (short)val);
-    if constexpr (sizeof(T) == sizeof(long))
-        return _InterlockedXor((volatile long*)mem, (long)val);
-    if constexpr (sizeof(T) == sizeof(long long))
-        return _InterlockedXor64((volatile long long*)mem, (long long)val);
-    MWR_UNREACHABLE;
-#endif
-}
-
-template <typename T, typename T2 = T>
-inline T atomic_and(T* mem, T2 val) {
-#ifndef MWR_MSVC
-    return __atomic_fetch_and(mem, (T)val, __ATOMIC_SEQ_CST);
-#else
-    if constexpr (sizeof(T) == sizeof(char))
-        return _InterlockedAnd8((volatile char*)mem, (char)val);
-    if constexpr (sizeof(T) == sizeof(short))
-        return _InterlockedAnd16((volatile short*)mem, (short)val);
-    if constexpr (sizeof(T) == sizeof(long))
-        return _InterlockedAnd((volatile long*)mem, (long)val);
-    if constexpr (sizeof(T) == sizeof(long long))
-        return _InterlockedAnd64((volatile long long*)mem, (long long)val);
-    MWR_UNREACHABLE;
-#endif
-}
-
-template <typename T, typename T2 = T>
-static T atomic_add(T* mem, T2 val) {
-#ifndef MWR_MSVC
-    return __atomic_fetch_add(mem, (T)val, __ATOMIC_SEQ_CST);
-#else
-    if constexpr (sizeof(T) == sizeof(char))
-        return _InterlockedExchangeAdd8((volatile char*)mem, (char)val);
-    if constexpr (sizeof(T) == sizeof(short))
-        return _InterlockedExchangeAdd16((volatile short*)mem, (short)val);
-    if constexpr (sizeof(T) == sizeof(long))
-        return _InterlockedExchangeAdd((volatile long*)mem, (long)val);
-    if constexpr (sizeof(T) == sizeof(long long))
-        return _InterlockedExchangeAdd64((volatile long long*)mem,
-                                         (long long)val);
-    MWR_UNREACHABLE;
-#endif
-}
-
-template <typename T, typename T2 = T>
-inline T atomic_min(T* mem, T2 val) {
-    T curr, next;
+template <typename T>
+inline T atomic_min(volatile void* mem, T val) {
     while (true) {
-        curr = *mem;
-        next = std::min<T>(curr, val);
-        if (atomic_cmpxchg(mem, curr, next))
+        T curr = read_once<T>(mem);
+        T next = std::min<T>(curr, val);
+        if (atomic_cas(mem, curr, next))
             return curr;
     };
 }
 
-template <typename T, typename T2 = T>
-inline T atomic_max(T* mem, T2 val) {
-    T curr, next;
+template <typename T>
+inline T atomic_max(volatile void* mem, T val) {
     while (true) {
-        curr = *mem;
-        next = std::max<T>(curr, val);
-        if (atomic_cmpxchg(mem, curr, next))
+        T curr = read_once<T>(mem);
+        T next = std::max<T>(curr, val);
+        if (atomic_cas(mem, curr, next))
             return curr;
     };
-}
-
-template <typename T, typename T2 = T>
-inline T atomic_swap(T* mem, T2 val) {
-#ifndef MWR_MSVC
-    return __atomic_exchange_n(mem, (T2)val, __ATOMIC_SEQ_CST);
-#else
-    if constexpr (sizeof(T) == sizeof(char))
-        return _InterlockedExchange8((volatile char*)mem, (char)val);
-    if constexpr (sizeof(T) == sizeof(short))
-        return _InterlockedExchange16((volatile short*)mem, (short)val);
-    if constexpr (sizeof(T) == sizeof(long))
-        return _InterlockedExchange((volatile long*)mem, (long)val);
-    if constexpr (sizeof(T) == sizeof(long long))
-        return _InterlockedExchange64((volatile long long*)mem,
-                                      (long long)val);
-    MWR_UNREACHABLE;
-#endif
-}
-
-template <typename FN>
-inline u64 atomic_dispatch(void* ptr, const void* val, size_t size, FN&& fn) {
-    switch (size) {
-    case 1:
-        return fn((u8*)ptr, *(const u8*)val);
-
-    case 2:
-        return fn((u16*)ptr, *(const u16*)val);
-
-    case 4:
-        return fn((u32*)ptr, *(const u32*)val);
-
-    case 8:
-        return fn((u64*)ptr, *(const u64*)val);
-
-    default:
-        MWR_ERROR("unsupported operand size: %zu bytes", size);
-    }
-}
-
-inline u64 atomic_or_ptr(void* ptr, const void* val, size_t size) {
-    return atomic_dispatch(ptr, val, size, [](auto* ptr, auto val) -> u64 {
-        return atomic_or(ptr, val);
-    });
-}
-
-inline u64 atomic_xor_ptr(void* ptr, const void* val, size_t size) {
-    return atomic_dispatch(ptr, val, size, [](auto* ptr, auto val) -> u64 {
-        return atomic_xor(ptr, val);
-    });
-}
-
-inline u64 atomic_and_ptr(void* ptr, const void* val, size_t size) {
-    return atomic_dispatch(ptr, val, size, [](auto* ptr, auto val) -> u64 {
-        return atomic_and(ptr, val);
-    });
-}
-
-inline u64 atomic_add_ptr(void* ptr, const void* val, size_t size) {
-    return atomic_dispatch(ptr, val, size, [](auto* ptr, auto val) -> u64 {
-        return atomic_add(ptr, val);
-    });
-}
-
-inline i64 atomic_min_ptr(void* ptr, const void* val, size_t size) {
-    return atomic_dispatch(ptr, val, size, [](auto* ptr, auto val) -> u64 {
-        typedef typename std::make_signed<decltype(val)>::type type;
-        return atomic_min((type*)ptr, (type)val);
-    });
-}
-
-inline i64 atomic_max_ptr(void* ptr, const void* val, size_t size) {
-    return atomic_dispatch(ptr, val, size, [](auto* ptr, auto val) -> u64 {
-        typedef typename std::make_signed<decltype(val)>::type type;
-        return atomic_max((type*)ptr, (type)val);
-    });
-}
-
-inline u64 atomic_umin_ptr(void* ptr, const void* val, size_t size) {
-    return atomic_dispatch(ptr, val, size, [](auto* ptr, auto val) -> u64 {
-        return atomic_min(ptr, val);
-    });
-}
-
-inline u64 atomic_umax_ptr(void* ptr, const void* val, size_t size) {
-    return atomic_dispatch(ptr, val, size, [](auto* ptr, auto val) -> u64 {
-        return atomic_max(ptr, val);
-    });
-}
-
-inline u64 atomic_swap_ptr(void* ptr, const void* val, size_t size) {
-    return atomic_dispatch(ptr, val, size, [](auto* ptr, auto val) -> u64 {
-        return atomic_swap(ptr, val);
-    });
-}
-
-inline bool atomic_cmpxchg(volatile void* ptr, u64 cmp, const void* val,
-                           size_t size) {
-    switch (size) {
-    case 1:
-        return atomic_cmpxchg((u8*)ptr, (u8)cmp, *(const u8*)val);
-
-    case 2:
-        return atomic_cmpxchg((u16*)ptr, (u16)cmp, *(const u16*)val);
-
-    case 4:
-        return atomic_cmpxchg((u32*)ptr, (u32)cmp, *(const u32*)val);
-
-    case 8:
-        return atomic_cmpxchg((u64*)ptr, (u64)cmp, *(const u64*)val);
-
-    default:
-        MWR_ERROR("unsupported operand size: %zu bytes", size);
-    }
 }
 
 } // namespace mwr
--- test/core/atomics.cpp
+++ test/core/atomics.cpp
@@ -13,81 +13,244 @@
 
 using namespace mwr;
 
-TEST(atomic, add) {
-    i32 val32 = 7;
-    EXPECT_EQ(atomic_add(&val32, 19), 7);
-    EXPECT_EQ(val32, 26);
+TEST(atomic, and8) {
+    u8 a = 0x50;
+    EXPECT_EQ(atomic_and8(&a, 0xe0), 0x50);
+    EXPECT_EQ(a, 0x50 & 0xe0);
 
-    i64 val64 = 12;
-    EXPECT_EQ(atomic_add(&val64, 5), 12);
-    EXPECT_EQ(val64, 17);
+    u8 c = 0x30;
+    u8 d = 0xf0;
+    EXPECT_EQ(atomic_and(&c, d), 0x30);
+    EXPECT_EQ(c, 0x30 & 0xf0);
 }
 
-TEST(atomic, max) {
-    i64 cmp, data = 5;
-    EXPECT_EQ(atomic_max(&data, 6), 5);
-    EXPECT_EQ(data, 6);
+TEST(atomic, and16) {
+    u16 a = 0x5000;
+    EXPECT_EQ(atomic_and16(&a, 0xe000), 0x5000);
+    EXPECT_EQ(a, 0x5000 & 0xe000);
 
-    EXPECT_EQ(atomic_max(&data, -1), 6);
-    EXPECT_EQ(data, 6);
-
-    cmp = -2;
-    EXPECT_EQ(atomic_max_ptr(&data, &cmp, sizeof(data)), 6);
-    EXPECT_EQ(data, 6);
-
-    cmp = 9;
-    EXPECT_EQ(atomic_max_ptr(&data, &cmp, sizeof(data)), 6);
-    EXPECT_EQ(data, 9);
+    u16 c = 0x3000;
+    u16 d = 0xf000;
+    EXPECT_EQ(atomic_and(&c, d), 0x3000);
+    EXPECT_EQ(c, 0x3000 & 0xf000);
 }
 
-TEST(atomic, umax) {
-    u64 cmp, data = 5;
-    EXPECT_EQ(atomic_max(&data, 6), 5);
-    EXPECT_EQ(data, 6);
+TEST(atomic, and32) {
+    u32 a = 0x50000000;
+    EXPECT_EQ(atomic_and32(&a, 0xe0000000), 0x50000000);
+    EXPECT_EQ(a, 0x50000000 & 0xe0000000);
 
-    EXPECT_EQ(atomic_max(&data, -2), 6);
-    EXPECT_EQ(data, -2);
-
-    cmp = -1;
-    EXPECT_EQ(atomic_umax_ptr(&data, &cmp, sizeof(data)), -2);
-    EXPECT_EQ(data, -1);
-
-    cmp = 9;
-    EXPECT_EQ(atomic_umax_ptr(&data, &cmp, sizeof(data)), -1);
-    EXPECT_EQ(data, -1);
+    u32 c = 0x30000000;
+    u32 d = 0xf0000000;
+    EXPECT_EQ(atomic_and(&c, d), 0x30000000);
+    EXPECT_EQ(c, 0x30000000 & 0xf0000000);
 }
 
-TEST(atomic, bitwise) {
-    u32 a = 1, b = 2, c = 4;
+TEST(atomic, and64) {
+    u64 a = 0x5000000000000000;
+    EXPECT_EQ(atomic_and64(&a, 0xe000000000000000), 0x5000000000000000);
+    EXPECT_EQ(a, 0x5000000000000000 & 0xe000000000000000);
 
-    ASSERT_EQ(atomic_or(&a, 2), 1);
-    EXPECT_EQ(a, 3);
-
-    ASSERT_EQ(atomic_and(&b, 2), 2);
-    EXPECT_EQ(b, 2);
-
-    ASSERT_EQ(atomic_xor(&c, 2), 4);
-    EXPECT_EQ(c, 6);
+    u64 c = 0x3000000000000000;
+    u64 d = 0xf000000000000000;
+    EXPECT_EQ(atomic_and(&c, d), 0x3000000000000000);
+    EXPECT_EQ(c, 0x3000000000000000 & 0xf000000000000000);
 }
 
-TEST(atomic, ptrops) {
-    const u64 data = -1;
-    u64 dest = 0;
+TEST(atomic, or8) {
+    u8 a = 0x50;
+    EXPECT_EQ(atomic_or8(&a, 0xa0), 0x50);
+    EXPECT_EQ(a, 0x50 | 0xa0);
 
-    EXPECT_EQ(atomic_add_ptr(&dest, &data, sizeof(u8)), 0);
-    EXPECT_EQ(dest, 0xff);
+    u8 c = 0x0f;
+    u8 d = 0xf0;
+    EXPECT_EQ(atomic_or(&c, d), 0x0f);
+    EXPECT_EQ(c, 0x0f | 0xf0);
+}
 
-    dest = 0;
-    EXPECT_EQ(atomic_add_ptr(&dest, &data, sizeof(u16)), 0);
-    EXPECT_EQ(dest, 0xffff);
+TEST(atomic, or16) {
+    u16 a = 0x5000;
+    EXPECT_EQ(atomic_or16(&a, 0xa000), 0x5000);
+    EXPECT_EQ(a, 0x5000 | 0xa000);
 
-    dest = 0;
-    EXPECT_EQ(atomic_add_ptr(&dest, &data, sizeof(u32)), 0);
-    EXPECT_EQ(dest, 0xffffffff);
+    u16 c = 0x0f00;
+    u16 d = 0xf000;
+    EXPECT_EQ(atomic_or(&c, d), 0x0f00);
+    EXPECT_EQ(c, 0x0f00 | 0xf000);
+}
 
-    dest = 0;
-    EXPECT_EQ(atomic_add_ptr(&dest, &data, sizeof(u64)), 0);
-    EXPECT_EQ(dest, 0xffffffffffffffff);
+TEST(atomic, or32) {
+    u32 a = 0x50000000;
+    EXPECT_EQ(atomic_or32(&a, 0xa0000000), 0x50000000);
+    EXPECT_EQ(a, 0x50000000 | 0xa0000000);
+
+    u32 c = 0x0f000000;
+    u32 d = 0xf0000000;
+    EXPECT_EQ(atomic_or(&c, d), 0x0f000000);
+    EXPECT_EQ(c, 0x0f000000 | 0xf0000000);
+}
+
+TEST(atomic, or64) {
+    u64 a = 0x5000000000000000;
+    EXPECT_EQ(atomic_or64(&a, 0xa000000000000000), 0x5000000000000000);
+    EXPECT_EQ(a, 0x5000000000000000 | 0xa000000000000000);
+
+    u64 c = 0x0f00000000000000;
+    u64 d = 0xf000000000000000;
+    EXPECT_EQ(atomic_or(&c, d), 0x0f00000000000000);
+    EXPECT_EQ(c, 0x0f00000000000000 | 0xf000000000000000);
+}
+
+TEST(atomic, xor8) {
+    u8 a = 0x5a;
+    EXPECT_EQ(atomic_xor8(&a, 0xa5), 0x5a);
+    EXPECT_EQ(a, 0x5a ^ 0xa5);
+
+    u8 c = 0x1f;
+    u8 d = 0xf1;
+    EXPECT_EQ(atomic_xor(&c, d), 0x1f);
+    EXPECT_EQ(c, 0x1f ^ 0xf1);
+}
+
+TEST(atomic, xor16) {
+    u16 a = 0x5a00;
+    EXPECT_EQ(atomic_xor16(&a, 0xa500), 0x5a00);
+    EXPECT_EQ(a, 0x5a00 ^ 0xa500);
+
+    u16 c = 0x1f00;
+    u16 d = 0xf100;
+    EXPECT_EQ(atomic_xor(&c, d), 0x1f00);
+    EXPECT_EQ(c, 0x1f00 ^ 0xf100);
+}
+
+TEST(atomic, xor32) {
+    u32 a = 0x5a000000;
+    EXPECT_EQ(atomic_xor32(&a, 0xa5000000), 0x5a000000);
+    EXPECT_EQ(a, 0x5a000000 ^ 0xa5000000);
+
+    u32 c = 0x1f000000;
+    u32 d = 0xf1000000;
+    EXPECT_EQ(atomic_xor(&c, d), 0x1f000000);
+    EXPECT_EQ(c, 0x1f000000 ^ 0xf1000000);
+}
+
+TEST(atomic, xor64) {
+    u64 a = 0x5a00000000000000;
+    EXPECT_EQ(atomic_xor64(&a, 0xa500000000000000), 0x5a00000000000000);
+    EXPECT_EQ(a, 0x5a00000000000000 ^ 0xa500000000000000);
+
+    u64 c = 0x1f00000000000000;
+    u64 d = 0xf100000000000000;
+    EXPECT_EQ(atomic_xor(&c, d), 0x1f00000000000000);
+    EXPECT_EQ(c, 0x1f00000000000000 ^ 0xf100000000000000);
+}
+
+TEST(atomic, add8) {
+    i8 a = -7;
+    EXPECT_EQ(atomic_add8(&a, 19), -7);
+    EXPECT_EQ(a, -7 + 19);
+
+    u8 b = 12;
+    EXPECT_EQ(atomic_add8(&b, -5), 12);
+    EXPECT_EQ(b, 12 - 5);
+
+    i8 c = -1;
+    u8 d = 14;
+    EXPECT_EQ(atomic_add(&c, d), 0xff);
+    EXPECT_EQ(c, -1 + 14);
+}
+
+TEST(atomic, add16) {
+    i16 a = -700;
+    EXPECT_EQ(atomic_add16(&a, 1900), -700);
+    EXPECT_EQ(a, -700 + 1900);
+
+    u16 b = 1200;
+    EXPECT_EQ(atomic_add16(&b, -500), 1200);
+    EXPECT_EQ(b, 1200 - 500);
+
+    i16 c = -1;
+    u16 d = 1400;
+    EXPECT_EQ(atomic_add(&c, d), 0xffff);
+    EXPECT_EQ(c, -1 + 1400);
+}
+
+TEST(atomic, add32) {
+    i32 a = -70000;
+    EXPECT_EQ(atomic_add32(&a, 190000), -70000);
+    EXPECT_EQ(a, -70000 + 190000);
+
+    u32 b = 120000;
+    EXPECT_EQ(atomic_add32(&b, -50000), 120000);
+    EXPECT_EQ(b, 120000 - 50000);
+
+    i32 c = -1;
+    u32 d = 140000;
+    EXPECT_EQ(atomic_add(&c, d), 0xffffffff);
+    EXPECT_EQ(c, -1 + 140000);
+}
+
+TEST(atomic, add64) {
+    i64 a = -70000000000;
+    EXPECT_EQ(atomic_add64(&a, 190000000000), -70000000000);
+    EXPECT_EQ(a, -70000000000 + 190000000000);
+
+    u64 b = 120000000000;
+    EXPECT_EQ(atomic_add64(&b, -50000000000), 120000000000);
+    EXPECT_EQ(b, 120000000000 - 50000000000);
+
+    i64 c = -1;
+    u64 d = 140000000000;
+    EXPECT_EQ(atomic_add(&c, d), 0xffffffffffffffff);
+    EXPECT_EQ(c, -1 + 140000000000);
+}
+
+TEST(atomic, swap8) {
+    u8 a = 0xab;
+    EXPECT_EQ(atomic_swap8(&a, 0xbc), 0xab);
+    EXPECT_EQ(a, 0xbc);
+
+    u8 b = 0xcd;
+    u8 c = 0xef;
+    EXPECT_EQ(atomic_swap(&b, c), 0xcd);
+    EXPECT_EQ(b, 0xef);
+    EXPECT_EQ(c, 0xef);
+}
+
+TEST(atomic, swap16) {
+    u16 a = 0xab00;
+    EXPECT_EQ(atomic_swap16(&a, 0xbc00), 0xab00);
+    EXPECT_EQ(a, 0xbc00);
+
+    u16 b = 0xcd00;
+    u16 c = 0xef00;
+    EXPECT_EQ(atomic_swap(&b, c), 0xcd00);
+    EXPECT_EQ(b, 0xef00);
+    EXPECT_EQ(c, 0xef00);
+}
+
+TEST(atomic, swap32) {
+    u32 a = 0xab000000;
+    EXPECT_EQ(atomic_swap32(&a, 0xbc000000), 0xab000000);
+    EXPECT_EQ(a, 0xbc000000);
+
+    u32 b = 0xcd000000;
+    u32 c = 0xef000000;
+    EXPECT_EQ(atomic_swap(&b, c), 0xcd000000);
+    EXPECT_EQ(b, 0xef000000);
+    EXPECT_EQ(c, 0xef000000);
+}
+
+TEST(atomic, swap64) {
+    u64 a = 0xab00000000000000;
+    EXPECT_EQ(atomic_swap64(&a, 0xbc00000000000000), 0xab00000000000000);
+    EXPECT_EQ(a, 0xbc00000000000000);
+
+    u64 b = 0xcd00000000000000;
+    u64 c = 0xef00000000000000;
+    EXPECT_EQ(atomic_swap(&b, c), 0xcd00000000000000);
+    EXPECT_EQ(b, 0xef00000000000000);
+    EXPECT_EQ(c, 0xef00000000000000);
 }
 
 TEST(atomic, cas8) {
@@ -147,4 +310,24 @@ TEST(atomic, cas128) {
     ASSERT_FALSE(atomic_cas(data, cmpv, wval, 16));
     ASSERT_EQ(data[0], wval[0]);
     ASSERT_EQ(data[1], wval[1]);
+}
+
+TEST(atomic, min) {
+    u32 a = 123;
+    EXPECT_EQ(atomic_min(&a, 32), 123);
+    EXPECT_EQ(a, 32);
+    EXPECT_EQ(atomic_min(&a, 0xfffffffe), 32);
+    EXPECT_EQ(a, 32);
+    EXPECT_EQ(atomic_min(&a, -2), 32); // uses signed version
+    EXPECT_EQ(a, 0xfffffffe);
+}
+
+TEST(atomic, max) {
+    u32 a = 123;
+    EXPECT_EQ(atomic_max(&a, 132), 123);
+    EXPECT_EQ(a, 132);
+    EXPECT_EQ(atomic_max(&a, -2), 132); // uses signed version
+    EXPECT_EQ(a, 132);
+    EXPECT_EQ(atomic_max(&a, 0xfffffffe), 132);
+    EXPECT_EQ(a, 0xfffffffe);
 }
