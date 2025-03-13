867c29d166e9a83fdf0ca671786431708130492c
Simon Winther <simon@mwa.re>
2025-03-13 14:04:57 +0100

    fix error

--- include/mwr/core/bitfields.h
+++ include/mwr/core/bitfields.h
@@ -29,7 +29,7 @@ template <typename T, typename RET = std::make_signed_t<T>>
 constexpr RET sextract(T val, size_t start, size_t w) {
     using U = std::make_unsigned_t<RET>;
     assert(start + w <= width_of(val) && "bit range exceeded");
-    assert(width && "width cannot be zero");
+    assert(w && "width cannot be zero");
     return (RET)((U)val << (width_of(val) - w - start)) >> (width_of(val) - w);
 }
 
