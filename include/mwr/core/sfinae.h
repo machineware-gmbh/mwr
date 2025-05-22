e3696549b593e7c1461775689c38c0fec6ac0580
Jan Henrik Weinstock <jan@mwa.re>
2025-05-22 12:46:10 +0200

    sfinae: adding member checker

--- include/mwr/core/sfinae.h
+++ include/mwr/core/sfinae.h
@@ -27,6 +27,18 @@ using enable_if_unsigned = typename std::enable_if<std::is_unsigned_v<T>, T>;
 template <typename T>
 using enable_if_unsigned_t = typename enable_if_unsigned<T>::type;
 
+#define MWR_DECL_MEMBER_CHECKER(T, MEMBER)                     \
+    struct member_checker_##T##_has_##MEMBER {                 \
+        template <typename T, typename U = void>               \
+        struct has_member : std::false_type {};                \
+        template <typename T>                                  \
+        struct has_member<T, std::void_t<decltype(T::MEMBER)>> \
+            : std::true_type {};                               \
+        constexpr operator bool() {                            \
+            return has_member<T>::value;                       \
+        }                                                      \
+    }
+
 } // namespace mwr
 
 #endif
--- test/core/sfinae.cpp
+++ test/core/sfinae.cpp
@@ -27,3 +27,29 @@ TEST(sfinae, is_signed) {
     EXPECT_EQ(test_is_signed(14), -14);
     EXPECT_EQ(test_is_signed(100u), 101u);
 }
+
+struct my_struct {
+    int a;
+    std::string s;
+};
+
+using my_struct_has_a = MWR_DECL_MEMBER_CHECKER(my_struct, a);
+using my_struct_has_b = MWR_DECL_MEMBER_CHECKER(my_struct, b);
+using my_struct_has_s = MWR_DECL_MEMBER_CHECKER(my_struct, s);
+
+TEST(sfinae, has_member) {
+    if constexpr (!my_struct_has_a())
+        GTEST_FAIL();
+    if constexpr (my_struct_has_b())
+        GTEST_FAIL();
+    if constexpr (!my_struct_has_s())
+        GTEST_FAIL();
+
+    bool a = my_struct_has_a();
+    bool b = my_struct_has_b();
+    bool s = my_struct_has_s();
+
+    EXPECT_TRUE(a);
+    EXPECT_FALSE(b);
+    EXPECT_TRUE(s);
+}
